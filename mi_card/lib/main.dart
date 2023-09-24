d7abe0e01b9338992099075005f3dc474cd5648a
MadhanV <madhanbalajiv@outlook.com>
2023-09-24 19:44:12 +0530

    Increases The number fontsize Removed the Floating button and replaced it with a Elavated button

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-      
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -47,34 +46,42 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-       
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title,),
+        title: Text(
+          widget.title,
+        ),
         centerTitle: true,
       ),
       body: Center(
-       
         child: Column(
-         
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Here is the Random number:',
+              'A random number',
+            ),
+            const SizedBox(
+              height: 60,
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                    fontSize: 240,
+                  ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: _randomNumGenerator,
+              child: const Icon(Icons.add_outlined),
+              onHover: (value) =>const Tooltip(message: 'Press',),
+              
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _randomNumGenerator,
-        tooltip: 'Press Here to generate',
-        child: const Icon(Icons.add),
-      ), 
     );
   }
 }
